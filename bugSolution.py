1603da730d76ec1c08b7137662b9d883dafcb900
BugHunter-X <askzenai@gmail.com>
2025-01-19 10:22:04 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,8 @@
+def function_with_error_handling(x):
+    try:
+        if x == 0:
+            return 1 / x
+        else:
+            return x + 1
+    except ZeroDivisionError:
+        return float('inf')  # Or any other appropriate handling
